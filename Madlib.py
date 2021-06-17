96d15c38d4f494dafad7c5920a6ec73c1dfcc664
Benjamin <bmotley901@gmail.com>
2021-06-16 21:39:47 -0400

    First Python Project

---
+++ Madlib.py
@@ -0,0 +1,14 @@
+
+#youtuber = "Benjamin Motley"
+
+#print("subscribe to " + youtuber)
+#print("subscribe to {}".format(youtuber))
+#print(f"subscribe to {youtuber}")
+
+adj = input("Adjectives: ")
+verb1 = input("Verb is: ")
+verb2 = input("Verb is: ")
+famous_person = input("Famous person is: ")
+madlib = f"Computer programming is so {adj}! It make me so excited all the time because\
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+print(madlib)
