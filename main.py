a163a74eba5327a9f6b51a6940f410d1455fe1a2
Benjamin <bmotley901@gmail.com>
2021-06-17 20:43:32 -0400

    Computer guesses number and I choose whether number is high or low

--- main.py
+++ main.py
@@ -13,6 +13,25 @@ def guess (x):
         print("Congrats you won the lottery")
         print(guess)
 
-guess(10)
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)??').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+
+    print(f"Congrats you got it robot, The number was {guess}")
+
+computer_guess(10)
 
 
